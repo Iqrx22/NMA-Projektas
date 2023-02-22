e6fe6c3d5b2ccbfb6983624a8f7e36798a359de3
Iqrx22 <sigitas.skersys@gmail.com>
2023-02-22 20:57:11 +0200

    Created a window

--- Zaidimai/Main.cpp
+++ Zaidimai/Main.cpp
@@ -8,11 +8,14 @@ const int WINDOW_LENGTH = 800, WINDOW_HEIGHT = 800;
 
 
 int main() {
+	//Initialising GLFW
 	glfwInit();
+	//Using GLFW version 3,3
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+	//Creating a window which name is FlappyBird
 	GLFWwindow* window = glfwCreateWindow(WINDOW_LENGTH, WINDOW_HEIGHT, "FlappyBird", NULL, NULL);
 	if (window == NULL) {
 		std::cout << "Nesukure window" << std::endl;
@@ -24,7 +27,7 @@ int main() {
 	gladLoadGL();
 	glViewport(0, 0, WINDOW_LENGTH, WINDOW_HEIGHT);
 
-	glClearColor(0.07f, 0.013f, 1.17f, 1.0f);
+	glClearColor(0.07f, 0.13f, 1.17f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
 	glfwSwapBuffers(window);
 
